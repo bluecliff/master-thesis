a6d4a516418f0bc99a5335074f87c9434b8fdd91
lishuangjiang <li0shuangjiang@gmail.com>
2015-01-04 20:44:06 +0800

    update

--- multthread_time.py
+++ multthread_time.py
@@ -3,12 +3,12 @@
 
 from pylab import *
 #多线程比对的时间和空间
-working_time_35=[201,114,114,111,111,110]
-working_memory_35=[3235278848,3274301440,3274747904,3275419648,3286237184,3286425600]
-working_time_70=[460,254,256,253,256,254]
-working_memory_70=[3268591616,3338215424,3342258176,3356774400,3370038016,3386941440]
-working_memory_125=[3402596352,3482193920,3511316480,3561840640,3591553024,3814600704]
-working_time_125=[1071,586,590,590,589,592]
+working_time_35=[1851,935,480,267,270,268]
+working_memory_35=[3330,3382,3467,3565,3597,3626]
+working_time_70=[4580,2307,1169,598,598,602]
+working_memory_70=[3384,3463,3551,3649,3658,3695]
+working_memory_125=[3447,3538,3641,3723,3744,3771]
+working_time_125=[12378,6197,3113,1564,1562,1567]
 
 
 #mpl.rcParams['font.sans-serif'] = ['Adobe Fangsong Std'] #指定默认字体
@@ -22,19 +22,19 @@ plot(xrange(1,7),working_time_125,'v-',label=u'125bp')
 xlim(0,7)
 xlabel(u'Thread Count')
 xticks(xrange(1,7),xrange(1,7))
-ylim(50,1100)
+#ylim(50,1100)
 ylabel(u"Time(s)")
 legend(loc="upper right")
 #show()
 #figure()
 subplot(1,2,2)
-plot(xrange(1,7),[item/1024.0/1024.0 for item in working_memory_35],'o-',label="35bp")
-plot(xrange(1,7),[item/1024.0/1024.0 for item in working_memory_70],'x-',label="70bp")
-plot(xrange(1,7),[item/1024.0/1024.0 for item in working_memory_125],'v-',label="125bp")
+plot(xrange(1,7),[item for item in working_memory_35],'o-',label="35bp")
+plot(xrange(1,7),[item for item in working_memory_70],'x-',label="70bp")
+plot(xrange(1,7),[item for item in working_memory_125],'v-',label="125bp")
 xlim(0,7)
 xlabel(u'Thread Count')
 xticks(xrange(1,7),xrange(1,7))
-ylim(3000,3650)
+#ylim(3000,3650)
 ylabel(u"Memory(MB)")
 legend(loc="upper left")
 show()
